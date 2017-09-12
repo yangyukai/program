8e01ea33c8ccf8779d4ebee381eba4c2e5441d85
yangyukai <31695399+yangyukai@users.noreply.github.com>
2017-09-12 17:00:05 +0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,8 @@
+#include<iostream.h>
+void main()
+{
+int i,a;
+i=5;
+a=i++;
+cout<<i<<endl<<a<<endl;
+}
---
+++ 2.cpp
@@ -0,0 +1,8 @@
+#include<iostream.h>
+void main()
+{
+int i,a;
+i=8;
+a=i++;
+cout<<i<<endl<<a<<endl;
+}
